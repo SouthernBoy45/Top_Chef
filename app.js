e5077f897cf5c6ee748bdb7007751b2ddff5a0c2
Colten Bowdoin <coltenbowdoin@gmail.com>
2023-08-21 11:03:16 -0500

    Search by Ingredient complete.

--- app.js
+++ app.js
@@ -167,11 +167,18 @@ function searchCuisines(allDishes) {
     alert("Found all dishes matching the cuisine search term!  Check the console for full output")
     return results
 }
-
+// TODO #4: Gather user input for an ingredient to search for, then filter for all dishes that INCLUDE this ingredient in their 
+// ingredients array property
 function searchIngredients(allDishes) {
     alert("Searching for dishes by ingredient...")
-    // TODO #4: Gather user input for an ingredient to search for, then filter for all dishes that INCLUDE this ingredient in their ingredients array property
+    let userInput = prompt("Which ingredient would like to search for?")
+    let results = allDishes.filter(function(el){
+        if (el.ingredients.includes(userInput)){
+            return true;
+        }
+    })
     alert("Found all dishes that contain the ingredient search term!  Check the console for full output")
+    return results
 }
 
 function generateCuisineDishName(allDishes) {
